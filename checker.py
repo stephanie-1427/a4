e95685bdd3549bcdf2bff53a3232e47ffd396ca0
Steph <stephlee2024@gmail.com>
2025-02-24 16:30:32 -0700

    first commit

---
+++ checker.py
@@ -0,0 +1,84 @@
+# checker.py
+
+# Error checking functions
+
+# Stephanie Lee
+# stephl25@uci.edu
+# 79834162
+
+from pathlib import Path
+
+
+class OptionTypeError(Exception):
+    pass
+
+
+def check_required_attributes(attribute: str):
+    if attribute is None or len(attribute) == 0 or attribute.isspace():
+        raise SyntaxError
+
+
+def check_option(option, supported_options: set):
+    if option not in supported_options:
+        raise OptionTypeError
+
+
+def check_whitespace(entry: str):
+    if entry.isspace() or len(entry) == 0:
+        raise SyntaxError
+
+
+def check_empty_entries(entry: str, valid_options: set):
+    if entry in valid_options:
+        raise SyntaxError
+
+
+def check_dict_edge_cases(valid_entries: dict, raw_entries: list):
+    if (len(valid_entries) == 0 or
+            ((len(valid_entries.keys()) + len(valid_entries.values()))
+                < len(raw_entries))):
+        if not raw_entries[-1].startswith("-"):
+            raise TypeError
+        else:
+            raise SyntaxError
+
+
+def check_length(command: str, command_line_input: list):
+    if ((command == 'C' and len(command_line_input) != 4) or
+            (command in ('O', 'D', 'R') and len(command_line_input) != 2)):
+        raise IndexError
+
+
+def check_int(x):
+    try:
+        int(x)
+        return
+    except ValueError:
+        raise ValueError
+
+
+def check_existence(path: Path):
+    if not path.exists():
+        raise FileNotFoundError
+
+
+def check_suffix(file_suffix):
+    suffix = '.dsu'
+    if not file_suffix == suffix:
+        # File type exception
+        raise TypeError
+
+
+def check_file_is_loaded(file):
+    if file is None:
+        raise FileNotFoundError
+
+
+def check_index(index: int, list_length: int):
+    if not (0 <= index < list_length):
+        raise IndexError
+
+
+def check_index_special(valid_index: bool):
+    if not valid_index:
+        raise IndexError
---
+++ ds_protocol.py
@@ -0,0 +1,178 @@
+# ds_protocol.py
+
+# Stephanie Lee
+# stephl25@uci.edu
+# 79834162
+
+import time
+import json
+from collections import namedtuple
+import socket
+
+# Namedtuple to hold the values retrieved from json messages.
+DataTuple = namedtuple('DataTuple', ['type', 'response'])
+DSConnection = namedtuple('DSConnection', ['socket', 'send', 'recv', 'token', 'timestamp'])
+
+
+class DSProtocolError(Exception):
+    '''
+    Error for when the socket fails to connect
+    '''
+    pass
+
+
+class ErrorMessage(Exception):
+    '''
+    Error for when the server returns an 'error' message type
+    '''
+    pass
+
+
+class InvalidEntry(Exception):
+    '''
+    Error for when an entry has an invalid value: None (except bio), "", or " "
+    '''
+    pass
+
+
+def extract_json(json_msg: str) -> DataTuple:
+    '''
+    Call the json.loads function on a json string and convert it to a DataTuple object
+
+    :param json_msg: The json string message to extract data from.
+    '''
+    try:
+        json_obj = json.loads(json_msg)
+        response = json_obj['response']
+        type = json_obj['response']['type']
+    except json.JSONDecodeError:
+        print("Json cannot be decoded.")
+
+    return DataTuple(type, response)
+
+
+def init(sock: socket) -> DSConnection:
+    '''
+    Set up communication between the client and server.
+    Establish a file to send data to the server.
+    Establish a file to receive data from the server.
+    Raise DSProtocolError when the client fails to connect to a server.
+
+    :param sock: Initialized socket.
+    '''
+    try:
+        f_send = sock.makefile('w')
+        f_recv = sock.makefile('r')
+    except Exception:
+        raise DSProtocolError("Invalid socket connection")
+    return DSConnection(
+            socket=sock,
+            send=f_send,
+            recv=f_recv,
+            token=None,
+            timestamp=None
+            )
+
+
+def write(ds_conn: DSConnection, send_to_server: dict):
+    '''
+    Send a json message to the server
+
+    :param ds_conn: The current DSConnection namedtuple that connects client to the server.
+    :param send_to_server: The json formatted message to be sent to the server.
+    '''
+    try:
+        json_msg = json.dumps(send_to_server)
+        ds_conn.send.write(json_msg + '\r\n')
+        ds_conn.send.flush()
+    except Exception:
+        raise DSProtocolError
+
+
+def response(ds_conn: DSConnection):
+    '''
+    Receive a json formatted message from the server and return it as a string
+
+    :param ds_conn: The current DSConnection namedtuple that connects client to the server.
+    '''
+    command_to_client = ds_conn.recv.readline()[:-1]
+    return command_to_client
+
+
+def read_msg(ds_conn: DSConnection):
+    '''
+    Wrapper for response
+
+    :param ds_conn: The current DSConnection namedtuple that connects client to the server.
+    '''
+    return response(ds_conn)
+
+
+#TODO: fix this call
+def read_data(server_msg: str):
+    '''
+    Wrapper for extract_json
+
+    :param server_msg: The raw server message formatted as a json string.
+    '''
+    return extract_json(server_msg)
+
+
+def format_directmsg(user_token: str, message: str, recipient: str):
+    '''
+    '''
+    msg_dict = {"token": user_token, "directmessage": {"entry": message, "recipient": recipient, "timestamp": time.time()}}
+    return msg_dict
+
+
+def format_new(user_token: str):
+    '''
+    '''
+    new_dict = {"token": user_token, "directmessage": "new"}
+    return new_dict
+
+
+def format_all(user_token: str):
+    '''
+    '''
+    all_dict = {"token": user_token, "directmessage": "all"}
+    return all_dict
+
+
+def format_join(user=None, password=None, token=None):
+    '''
+    Format a join message and return the message as join_dict
+
+    :param user: The user name collected or used to call.
+    :param password: The password associated with the user name.
+    :param token: The current token. "" will be passed until further notice.
+    '''
+    join_dict = {"join": {"username": user, "password": password, "token": token}}
+    return join_dict
+
+
+def get_server_message(data: DataTuple):
+    '''
+    Return server message
+
+    :param data: DataTuple of the server message "response" and "token".
+    '''
+    return data.response['message']
+
+
+def get_token(data: DataTuple):
+    '''
+    Return the current client token
+
+    :param data: DataTuple of the server message "response" and "token".
+    '''
+    return data.response['token']
+
+
+def get_msg_type(data: DataTuple):
+    '''
+    Return the server message type ('ok' or 'error')
+
+    :param data: DataTuple of the server message "response" and "token".
+    '''
+    return data.type
